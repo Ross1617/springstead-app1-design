aa84dca406c7cd5b2c79d329b648a6e6b5f3e6f3
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-10-24 21:04:13 -0400

    finished

--- app1/src/test/java/baseline/editDescriptionTest.java
+++ app1/src/test/java/baseline/editDescriptionTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class editDescriptionTest {
     //create new instance of editDescription
     @Test
-    public void test_editDescription(){
+    public void test_editDescription() {
         //create an expected ArrayList
         //call editDescription with the parameters completed
         //assign the return value to  an Array list
@@ -15,3 +15,4 @@ class editDescriptionTest {
 
 
     }
+}
